86d0e3a98681c4738c5a280515b7d60f1234aed9
kanimozhi67 <prettykani67@gmail.com>
2025-12-12 09:53:09 +0400

    await in login

--- db/connectdb.js
+++ db/connectdb.js
@@ -4,6 +4,9 @@ const connectdb = async() => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
+    console.log("MONGO_URL ->", process.env.MONGO_URL);
+console.log("DB Ready:", mongoose.connection.readyState);
+
   } catch (err) {
     console.log(err);
     process.exit(1);
