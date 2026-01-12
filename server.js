5017a894565240082a5c4e32d543c679a15104c8
kanimozhi67 <prettykani67@gmail.com>
2026-01-12 16:10:46 +0400

    https

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000","http://localhost:5173",
-      "http://flairfrontend.vercel.app",
+      "https://flairfrontend.vercel.app",
       process.env.FRONTEND_URL,
     ], // your frontend URL
      methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
